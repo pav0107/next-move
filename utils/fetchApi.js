90158bb6b799feee39a02abadb190ff3792a0bc0
Pav <pav0107@gmail.com>
2022-10-17 08:11:18 +0100

    hide API key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      'X-RapidAPI-Key': '3c5dbc95f9mshdde1449d0344898p1b241ejsna13157c3afa0',
+      'X-RapidAPI-Key': process.env.KEY,
       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
     },
   });
